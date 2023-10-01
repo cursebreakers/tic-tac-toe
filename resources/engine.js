6be82361a7307117f9ad340b8e8b8c896122eb47
cursebreakers <esgvtt@gmail.com>
2023-10-01 15:46:26 -0400

    base mechanincs established

--- resources/engine.js
+++ resources/engine.js
@@ -1,27 +1,37 @@
 
-
 // Event listener for page-load
 window.addEventListener("load", (event) => {
-    // Report game engine status
-    console.log('Engine: Initializing...');  
       loadEngine()
 });
 
 // Game loader function
 function loadEngine() {
-    console.log('Engine: Waiting...')
-
+    console.log('Engine: Online')
+    
+    // Define game elements
     const gameTray = document.getElementById("gameTray");
+    const startButton = document.getElementById("startButton");
+    const message = document.getElementById("message");
+    const board = document.getElementById("board");
+    const tray = document.getElementById('gameTray')
+
+    // Game settings
+    const gridSize = 3;
+    let currentPlayer = 1; // Player 1 starts
+
+    // Game state object to store relevant data
+    const gameState = {
+        player1: {},
+        player2: {},
+        currentPlayer: 1,
+        boardState: [],
+        gameEnded: false,
+    };
     
     if (!gameTray) {
         console.error("gameTray element not found");
         return; // Exit the function if gameTray is not found
     }
-
-    const startButton = document.getElementById("startButton");
-    const message = document.getElementById("message");
-    const board = document.getElementById("board");
-    const tray = document.getElementById('gameTray')
 
     // Check if the startButton element exists
     if (startButton) {
@@ -32,11 +42,8 @@ function loadEngine() {
 
     function initGame() {
         // Report game engine status
-        console.log('Engine: Initialized');  
-        // Update game message
-        if (message) {
-            message.textContent = "Player select";
-        }
+        console.log('Engine: Initializing...');
+
         //Clear game board
         if (board) {
             board.innerHTML = '';
@@ -47,37 +54,42 @@ function loadEngine() {
             startButton.addEventListener("click", () => {
                 location.reload(); // Reload the page when the button is clicked
             });
+            
+            setupPlayers();
+            createBoard();            
         }
+    }    
 
-        function setupPlayers() {
-          // Report game engine status
-          console.log('Engine: Running player creation...'); 
+    function setupPlayers() {
+        // Report game engine status
+        console.log('Engine: Running player creation...'); 
 
-          // Create input elements for player names and choices
-          const player1NameIn = document.createElement("input");
-          player1NameIn.placeholder = "Enter Player 1's name";
-          const player1Choice = document.createElement("select");
-          player1Choice.innerHTML = '<option value="x">X</option><option value="o">O</option>';
+        // Create input elements for player names and choices
+        const player1NameIn = document.createElement("input");
+        player1NameIn.placeholder = "Enter Player 1's name";
+        const player1Choice = document.createElement("select");
+        player1Choice.innerHTML = '<option value="x">X</option><option value="o">O</option>';
 
-          const player2NameIn = document.createElement("input");
-          player2NameIn.placeholder = "Enter Player 2's name";
-          const player2Choice = document.createElement("select");
-          player2Choice.innerHTML = '<option value="x">X</option><option value="o" selected>O</option>';
-            // Add the input elements to the board
-            if (tray) {
-                tray.appendChild(player1NameIn);
-                tray.appendChild(player1Choice);
-                tray.appendChild(player2NameIn);
-                tray.appendChild(player2Choice);
+        const player2NameIn = document.createElement("input");
+        player2NameIn.placeholder = "Enter Player 2's name";
+        const player2Choice = document.createElement("select");
+        player2Choice.innerHTML = '<option value="x">X</option><option value="o" selected>O</option>';
+            
+        // Add the input elements to the board
+        if (tray) {
+            tray.appendChild(player1NameIn);
+            tray.appendChild(player1Choice);
+            tray.appendChild(player2NameIn);
+            tray.appendChild(player2Choice);
         
-                // Add a button to start the game after collecting inputs
-                const playButton = document.createElement("button");
-                playButton.textContent = "Play!";
-                playButton.addEventListener("click", () => {
-                    const player1Name = player1NameIn.value;
-                    const player1Piece = player1Choice.value;
-                    const player2Name = player2NameIn.value;
-                    const player2Piece = player2Choice.value;
+            // Add a button to start the game after collecting inputs
+            const playButton = document.createElement("button");
+            playButton.textContent = "Play!";
+            playButton.addEventListener("click", () => {
+                const player1Name = player1NameIn.value;
+                const player1Piece = player1Choice.value;
+                const player2Name = player2NameIn.value;
+                const player2Piece = player2Choice.value;
         
                 // Check if inputs are valid
                 if (!player1Name || !player2Name) {
@@ -85,29 +97,63 @@ function loadEngine() {
                     return;
                 }
 
-                    // Now you have player names and choices, continue with the game setup
-                    runGame(player1Name, player1Piece, player2Name, player2Piece);
-                        
-                        // Remove the player select inputs from the tray
-                        if (tray) {
-                            tray.removeChild(player1NameIn);
-                            tray.removeChild(player1Choice);
-                            tray.removeChild(player2NameIn);
-                            tray.removeChild(player2Choice);
-                            tray.removeChild(playButton);
-                        }
-                        
-                    });
-                            tray.appendChild(playButton);
-                }
+                // Now you have player names and choices, continue with the game setup
+                gameState.player1 = { name: player1Name, piece: player1Piece };
+                gameState.player2 = { name: player2Name, piece: player2Piece };
+                gameState.currentPlayer = 1;
+                gameState.boardState = initializeBoard();
+                gameState.gameEnded = false;
+
+                runGame();
+            });
+
+            tray.appendChild(playButton);
+        }
+    }
+
+            
+    // Initialize the game board
+    function createBoard() {
+        // Report game engine status
+        console.log('Engine: Loading board...');
+        for (let row = 0; row < gridSize; row++) {
+            const rowArray = [];
+            for (let col = 0; col < gridSize; col++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.dataset.row = row;
+                cell.dataset.col = col;
+                cell.addEventListener("click", handleCellClick);
+                board.appendChild(cell);
+                rowArray.push(null);
             }
+            gameState.boardState.push(rowArray);
+        }
+    }
 
-        setupPlayers()           
+    function initializeBoard() {
+        const boardState = [];
+        for (let row = 0; row < gridSize; row++) {
+            const rowArray = [];
+            for (let col = 0; col < gridSize; col++) {
+                rowArray.push(null);
+            }
+            boardState.push(rowArray);
+        }
+        return boardState;
+    }
 
+
+    // Game logic
     function runGame(player1Name, player1Piece, player2Name, player2Piece) {
         console.log(player1Piece, player1Name)
-        console.log(player2Piece, player2Name)   
-    // Create character objects for both players
+        console.log(player2Piece, player2Name)  
+        // Update game message
+        if (message) {
+            message.textContent = `Move: ${gameState.player1.piece} ${gameState.player1.name}`;
+        }
+
+        // Create character objects for both players
         const player1 = {
             name: player1Name,
             piece: player1Piece,
@@ -127,89 +173,120 @@ function loadEngine() {
         if (message) {
         message.textContent = "Move:" + " " + player1Piece + " " + player1Name
         }
+        console.log('Game ready')
     }
-      
-    function createBoard() {
-        // Report game engine status
-        console.log('Engine: Loading board logic...');
-        const gridSize = 3; // You can change this to modify the size of the grid
-        for (let row = 0; row < gridSize; row++) {
-            for (let col = 0; col < gridSize; col++) {
-                const cell = document.createElement("div");
-                cell.classList.add("cell");
-                cell.dataset.row = row;
-                cell.dataset.col = col;
-                cell.addEventListener("click", handleCellClick);
-                board.appendChild(cell);
-            }
-        }
-    }
-
-    let currentPlayer = 1; // Player 1 starts
 
 
     function handleCellClick(event) {
         console.log('clicking cell...')
+
+        // Check if the game has ended
+        if (gameState.gameEnded) {
+            return;
+        }
+        
         const cell = event.target;
         const row = cell.dataset.row;
         const col = cell.dataset.col;
-        
+
         // Check if the cell is already occupied
-        if (cell.textContent !== '') {
+        if (gameState.boardState[row][col] !== null) {
             return; // Cell is already occupied, do nothing
         }
         
         // Mark the cell with the current player's piece
-        cell.textContent = currentPlayer === 1 ? 'X' : 'O';
+        cell.textContent = gameState.currentPlayer === 1 ? gameState.player1.piece : gameState.player2.piece;
+
+        // Update the board state
+        gameState.boardState[row][col] = gameState.currentPlayer;
 
         // Check if the current player has won
-        if (checkWin(currentPlayer === 1 ? 'X' : 'O', row, col)) {
-            message.textContent = `Player ${currentPlayer} (${currentPlayer === 1 ? 'X' : 'O'}) wins!`;
-            // You can add code here to end the game or reset the board.
+        if (checkWin(gameState.currentPlayer, row, col)) {
+            message.textContent = `Player ${gameState.currentPlayer} (${gameState.currentPlayer === 1 ? gameState.player1.piece : gameState.player2.piece}) wins!`;
+            gameState.gameEnded = true;
             return;
         }
 
         // Check if the game is a draw
         if (checkDraw()) {
-            message.textContent = 'It\'s a draw!';
-            // You can add code here to end the game or reset the board.
+            message.textContent = "It's a draw!";
+            gameState.gameEnded = true;
             return;
         }
 
         // Switch to the next player's turn
-        currentPlayer = currentPlayer === 1 ? 2 : 1;
-        message.textContent = `Move: ${currentPlayer === 1 ? 'X' : 'O'} Player ${currentPlayer}`;
+        gameState.currentPlayer = gameState.currentPlayer === 1 ? 2 : 1;
+        message.textContent = `Move: ${gameState.currentPlayer === 1 ? gameState.player1.piece : gameState.player2.piece} ${gameState.currentPlayer === 1 ? gameState.player1.name : gameState.player2.name}`;
     }
 
-    function checkWin(piece, row, col) {
-        console.log('Checking for win...')
-        // Implement your win condition logic here
-        // You need to check rows, columns, and diagonals to see if they contain the same piece.
-        // Return true if the current player has won, otherwise return false.
-        // You can use nested for loops to check rows, columns, and diagonals.
-        // Example win conditions:
-        // - Horizontal: [X, X, X] or [O, O, O]
-        // - Vertical: [X, X, X] or [O, O, O]
-        // - Diagonal: [X, X, X] or [O, O, O]
-    
-        // You can implement this logic as a separate function and call it from handleCellClick.
+        function checkWin(player, row, col) {
+            console.log('Checking for win...')
+
+            // Check for a win in the row
+        for (let i = 0; i < gridSize; i++) {
+            if (gameState.boardState[row][i] !== player) {
+                break; // If a cell in the row doesn't match, no win in this row
+            }
+            if (i === gridSize - 1) {
+                return true; // All cells in the row match, player wins
+            }
+        } 
+        
+        // Check for a win in the column
+        for (let i = 0; i < gridSize; i++) {
+            if (gameState.boardState[i][col] !== player) {
+                break; // If a cell in the column doesn't match, no win in this column
+            }
+            if (i === gridSize - 1) {
+                return true; // All cells in the column match, player wins
+            }
+        }
+        
+        // Check for a win in the main diagonal
+        if (row === col) {
+            for (let i = 0; i < gridSize; i++) {
+                if (gameState.boardState[i][i] !== player) {
+                    break; // If a cell in the diagonal doesn't match, no win in this diagonal
+                }
+                if (i === gridSize - 1) {
+                    return true; // All cells in the diagonal match, player wins
+                }
+            }
+        }
+
+        // Check for a win in the anti-diagonal (from top-right to bottom-left)
+        if (row + col === gridSize - 1) {
+            for (let i = 0; i < gridSize; i++) {
+                if (gameState.boardState[i][gridSize - 1 - i] !== player) {
+                    break; // If a cell in the anti-diagonal doesn't match, no win in this anti-diagonal
+                }
+                if (i === gridSize - 1) {
+                    return true; // All cells in the anti-diagonal match, player wins
+                }
+            }
+        }    
+
+        return false; // No win condition found
     }
 
     function checkDraw() {
         console.log('Checking for draw...')
-        // Implement your draw condition logic here
-        // Check if all cells are occupied and no player has won.
-        // Return true if it's a draw, otherwise return false.
-        // You can use nested for loops to iterate through all cells.
-    
-        // You can implement this logic as a separate function and call it from handleCellClick.
+        // Check if all cells are occupied
+        for (let row = 0; row < gridSize; row++) {
+            for (let col = 0; col < gridSize; col++) {
+                if (gameState.boardState[row][col] === null) {
+                    return false; // At least one cell is empty, game is not a draw
+                }
+            }
+        }
+        
+        return true; // All cells are occupied, game is a draw
     }
-    
 
+        // Report game engine status
+        console.log('Engine: Nominal'); 
     }
-          // Report game engine status
-          console.log('Engine: Nominal'); 
 
-}
-
+// Status check 1
+console.log('Engine: Boot...')
 
